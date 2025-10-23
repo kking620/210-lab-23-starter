4ab93e2125a61c607aef1f36dd3f2a4a832cc900
Krispy King <kking620@gmail.com>
2025-10-23 15:24:26 -0700

    Third commit

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main_menu();
 
 int main() {
     srand(time(0));
+    list<Goat> trip;
     bool again;
 
     // read & populate arrays for names and colors
@@ -29,9 +30,48 @@ int main() {
     while (fin1 >> colors[i++]);
     fin1.close();
 
+    
+    while(true){
+        int choice = main_menu();
 
-
+        if(choice == 1)
+            add_goat(trip, names, colors);
+        if(choice == 2)
+            delete_goat(trip);
+        if(choice == 3)
+            display_trip(trip);
+        if(choice == 4)
+            break;
+    }
 
     return 0;
+}
+
+int main_menu() {
+    int c;
+    
+    cout << "*** GOAT MANAGER 3001 ***\n";
+    cout << "[1] Add a goat\n";
+    cout << "[2] Delete a goat\n";
+    cout << "[3] List goats\n";
+    cout << "[4] Quit\n";
+    cout << "Choice -->";
+    cin >> c;
+
+    if(c >= 1 && c <=4)
+        return c;
+    else
+        cout << "Invalid input";
+}
+
+void display_trip(list<Goat> trip){
+    
+}
+
+void add_goat(list<Goat> &trip, string names[], string colors[]) {
+    string name = names[rand() % SZ_NAMES];
+    string color = colors[rand() % SZ_COLORS];
+    int age = rand() % MAX_AGE + 1;
+    trip.push_back(Goat(name, age, color));
 }
 
